b81dba8468a6839309afb1c60b7374c5987fe8e4
Lucas Lederman <ljlederman@gmail.com>
2021-09-07 17:43:49 +0100

    made new page for a interactive shopping list, not yet working, needs checking

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,23 @@
-const colorButton = document.getElementsByClassName("colorInput");
-const coloring = document.getElementsByClassName("colorBox");
+const colorButton = document.getElementById("colorInput");
+const coloring = document.getElementById("colorBox");
 const button = document.getElementById("subButton");
 
 button.addEventListener("click", () => {
-    coloring.style.color = colorButton.value;
-})
+    coloring.style.backgroundColor = colorButton.value;
+    colorButton.value = "";
+});
+
+//shopping list section for page 2
+const newItem = document.getElementById("newItem");
+const removeItem = document.getElementById("removeItem");
+const inputItem = document.getElementById("inputItem");
+let shoppingList = document.getElementById("shoppingList");
+
+newItem.addEventListener("click", () =>{
+    let listItem = document.createElement("li");
+    listItem.textContent = inputItem.value;
+    inputItem.value = "";
+    
+    let theList = document.getElementsByTagName("ul")[0];
+    theList.append(listItem)    
+});
